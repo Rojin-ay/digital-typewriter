3def1907cea4abc0fb1efbb3d4cfd52af3252166
Rojin Inan <inan.rojin21@gmail.com>
2026-05-23 02:29:19 +0200

    fix notion api query

--- api/notion.js
+++ api/notion.js
@@ -1,25 +1,19 @@
-const { Client } = require("@notionhq/client");
+import { Client } from "@notionhq/client"
 
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
-});
+})
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   try {
     const response = await notion.databases.query({
       database_id: process.env.NOTION_DATABASE_ID,
-    });
+    })
 
-    const todos = response.results.map((page) => ({
-      title: page.properties.Name.title[0]?.plain_text || "Untitled",
-      type: page.properties.Priority.select?.name || "todo",
-    }));
-
-    res.status(200).json(todos);
+    res.status(200).json(response.results)
   } catch (error) {
     res.status(500).json({
       error: error.message,
-    });
+    })
+  }
 }
-//no
-};
